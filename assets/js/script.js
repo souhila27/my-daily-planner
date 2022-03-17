b2f63652ed648d9ebacddf1b7a8816380d090fbf
souhila boukari <suhamimisou14@gmail.com>
2022-03-17 11:10:28 -0400

    adding color to the background in a if statment

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,32 @@
 var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY h:mm:ss A");
 $("#currentDay").text(currentDate);
 
+
+var current_task_color = '#ff6961';
+var future_task_color = '#77dd77';
+
+// on click of the save button
+// reach into the html
+// get the value of the text box that matches the button
+// store that in local storage
+var workDo = $(".time-go").toArray();
+function setTimeWorkDo() {
+    var currentHour = parseInt(moment().format('kk')); //current hour as a military time 
+    console.log(currentHour, workDo);
     
+    // for each time block
+    for (var i = 0; i < workDo.length; i++) {
+        var timeBlockHour = parseInt(workDo[i].id)
+        // if the timeblock matches the current hour
+        if (timeBlockHour === currentHour) {
+            // change that elements stype to red
+            workDo[i].style.backgroundColor = current_task_color;
+        } else if (timeBlockHour > currentHour) { // if timeblock is n the future
+            // change that element style to green
+            workDo[i].style.backgroundColor = future_task_color;
+        }
+    }
+}
 
-
+setTimeWorkDo();
 
